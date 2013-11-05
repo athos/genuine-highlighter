a0df1c489ca0f408caab63d10abcbff3099ccaf2
OHTA Shogo <athos0220@gmail.com>
2013-11-05 21:18:27 +0900

    first commit

---
+++ test/genuine_highlighter/core_test.clj
@@ -0,0 +1,7 @@
+(ns genuine-highlighter.core-test
+  (:require [clojure.test :refer :all]
+            [genuine-highlighter.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
