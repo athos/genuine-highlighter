f1255f231e6f66ee9702b4c4f011fc0f4e543c98
OHTA Shogo <athos0220@gmail.com>
2014-01-01 05:10:33 +0900

    parser skeleton

--- src/genuine_highlighter/parser.clj
+++ src/genuine_highlighter/parser.clj
@@ -17,6 +17,55 @@
 
 (defn- parse-comment [rdr initch])
 
+(defn- not-implemented [m]
+  (fn [& args]
+    (throw (Exception. (str "reader macro " m " not implemented yet")))))
+
+(defn- dispatch-macros [ch]
+  (case ch
+    \^ (not-implemented "#^") #_read-meta
+    \' (not-implemented "#'") #_(wrapping-reader 'var)
+    \( (not-implemented "#(") #_read-fn
+    \= (not-implemented "#=") #_read-eval
+    \{ (not-implemented "#{") #_read-set
+    \< (not-implemented "#<") #_(throwing-reader "Unreadable form")
+    \" (not-implemented "#\"") #_read-regex
+    \! (not-implemented "#!") #_read-comment
+    \_ (not-implemented "#_") #_read-discard
+    nil))
+
+(defn- read-dispatch
+  [rdr _]
+  (if-let [ch (types/read-char rdr)]
+    (if-let [dm (dispatch-macros ch)]
+      (dm rdr ch)
+      (types/reader-error rdr "No dispatch macro for " ch)
+      #_(if-let [obj (read-tagged (doto rdr (unread ch)) ch)]
+          obj
+          (types/reader-error rdr "No dispatch macro for " ch)))
+    (types/reader-error rdr "EOF while reading character")))
+
+(defn- macros [ch]
+  (case ch
+    \" (not-implemented \") #_read-string*
+    \: (not-implemented \:) #_read-keyword
+    \; (not-implemented \;) #_read-comment
+    \' (not-implemented \') #_(wrapping-reader 'quote)
+    \@ (not-implemented \@) #_(wrapping-reader 'clojure.core/deref)
+    \^ (not-implemented \^) #_read-meta
+    \` (not-implemented \`) #_read-syntax-quote ;;(wrapping-reader 'syntax-quote)
+    \~ (not-implemented \~) #_read-unquote
+    \( (not-implemented \() #_read-list
+    \) (not-implemented \)) #_read-unmatched-delimiter
+    \[ (not-implemented \[) #_read-vector
+    \] (not-implemented \]) #_read-unmatched-delimiter
+    \{ (not-implemented \{) #_read-map
+    \} (not-implemented \}) #_read-unmatched-delimiter
+    \\ (not-implemented \\) #_read-char*
+    \% (not-implemented \%) #_read-arg
+    \# read-dispatch
+    nil))
+
 (defn parse
   ([reader] (parse reader true))
   ([reader eof-error?]
@@ -32,7 +81,7 @@
                  (utils/comment-prefix? ch)
                  #_=> (cons (parse-comment reader ch) (parse reader eof-error?))
                  :else
-                 #_=> (let [f (#'r/macros ch)]
+                 #_=> (let [f (macros ch)]
                         (-> (if f
                               (let [res (f reader ch)]
                                 (if (identical? res reader)
