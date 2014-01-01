6a20a48888a0d02870bac6bebbca2a4ed723b1d9
OHTA Shogo <athos0220@gmail.com>
2014-01-01 17:28:18 +0900

    removed unnecessary files

--- test/genuine_highlighter/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns genuine-highlighter.core-test
-  (:require [clojure.test :refer :all]
-            [genuine-highlighter.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
