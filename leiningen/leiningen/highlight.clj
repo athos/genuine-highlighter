20e7b6858dee31cab00d07c0bd6836421841003f
OHTA Shogo <athos0220@gmail.com>
2014-02-23 06:10:08 +0900

    implement simple highlighting REPL

--- leiningen/leiningen/highlight.clj
+++ leiningen/leiningen/highlight.clj
@@ -1,6 +1,23 @@
-(ns leiningen.highlight)
+(ns leiningen.highlight
+  (:require [clojure.main :as m]
+            [genuine-highlighter.parser :as p]
+            [genuine-highlighter.analyzer :as a]
+            [genuine-highlighter.renderer :as r]
+            [genuine-highlighter.converter :as c]
+            [genuine-highlighter.decoration-rules.terminal :as t]))
+
+(defn- drop-proceeding-newlines [nodes]
+  (drop-while #(and (= (:type %) :whitespaces)
+                    (= (:raw %) "\n"))
+              nodes))
+
+(defn- read-and-highlight [in]
+  (let [p (drop-proceeding-newlines (p/parse in))]
+    (printf "  #_== %s\n" (r/render t/colorful-symbols-rule (a/analyze p)))
+    (c/convert p)))
 
 (defn highlight
-  "I don't do a lot."
+  "Highlight Clojure source code."
   [project & args]
-  (println "Hi!"))
+  (m/repl :read (fn [_ _] (read-and-highlight *in*))
+          :need-prompt #(do true)))
