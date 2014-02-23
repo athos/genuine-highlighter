360e8347a7cf053860861f92a431497e01ec2a65
OHTA Shogo <athos0220@gmail.com>
2014-02-23 18:26:35 +0900

    add Sjacket to the dependencies

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.reader "0.8.3"]]
+                 [org.clojure/tools.reader "0.8.3"]
+                 [org.clojars.trptcolin/sjacket "0.1.4" :exclusions [[org.clojure/clojure]]]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]]}})
