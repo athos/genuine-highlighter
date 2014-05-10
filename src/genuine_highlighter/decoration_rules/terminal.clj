56ce264df789a1bbbe46775e607ea28941de4866
OHTA Shogo <athos0220@gmail.com>
2014-05-10 20:56:48 +0900

    rewrite decoration rules for new format

--- src/genuine_highlighter/decoration_rules/terminal.clj
+++ src/genuine_highlighter/decoration_rules/terminal.clj
@@ -7,19 +7,18 @@
   (format "\033[38;5;%dm%s\033[0m" c x))
 
 (def white-symbols-rule
-  {:symbol {:symbol #(color-ansi 37 %2)}})
+  {:symbol {:content #(color-ansi 37 %2)}})
 
-(defn ^:private colorful-symbol [_]
-  (fn [x v]
-    (let [code {:special 36, :macro 33, :var 35, :local 34}
-          color (fn [x]
-                  (if-let [c (code (:type (:usage x)))]
-                    (color-ansi c v)
-                    (str (:symbol x))))]
-      (color x))))
+(defn ^:private colorful-symbol [x v]
+  (let [code {:special 36, :macro 33, :var 35, :local 34}
+        color (fn [x]
+                (if-let [c (code (:type (:usage x)))]
+                  (color-ansi c v)
+                  (str (:symbol x))))]
+    (color x)))
 
 (def colorful-symbols-rule
-  {:symbol colorful-symbol})
+  {:symbol {:content colorful-symbol}})
 
 (def rainbow-parens-rule
   (let [colors (atom (cycle (range 31 38)))
@@ -33,20 +32,20 @@
                    (let [[color] @color-stack]
                      (swap! color-stack rest)
                      (color-ansi color v)))]
-    {:list {:lparen open-fn, :rparen close-fn}
-     :vector {:lbracket open-fn, :rbracket close-fn}
-     :map {:lbrace open-fn, :rbrace close-fn}}))
+    {:list {:open open-fn, :close close-fn}
+     :vector {:open open-fn, :close close-fn}
+     :map {:open open-fn, :close close-fn}}))
 
 (def fadeout-parens-rule
   (letfn [(count-preceding-parens [x]
             (loop [x x n 0]
-              (if (#{:list :vector :map} (:type x))
+              (if (#{:list :vector :map} (:tag x))
                 (recur (last (:nodes x)) (inc n))
                 n)))
           (fadeout-parens [x v]
             (let [colors {1 255, 2 255, 3 247, 4 242}
                   color (or (colors (count-preceding-parens x)) 237)]
               (color-256 color v)))]
-    {:list {:rparen fadeout-parens}
-     :vector {:rbracket fadeout-parens}
-     :map {:rbrace fadeout-parens}}))
+    {:list {:close fadeout-parens}
+     :vector {:close fadeout-parens}
+     :map {:close fadeout-parens}}))
