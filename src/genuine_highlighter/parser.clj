93f2d774f398a18bcd23cb8871ae3f2ecc3543ad
OHTA Shogo <athos0220@gmail.com>
2014-06-30 22:24:25 +0900

    add unfinished?/unexpected? to parser

--- src/genuine_highlighter/parser.clj
+++ src/genuine_highlighter/parser.clj
@@ -32,3 +32,10 @@
 
 (defn node-content [node]
   (first (node-content* node)))
+
+(defn unfinished? [node]
+  (= (:tag node) :net.cgrand.parsley/unfinished))
+
+(defn unexpected? [node]
+  (some #(= (:tag %) :net.cgrand.parsley/unexpected)
+        (tree-seq node-tag node-content* node)))
