13c5e4124f719de20de8b0e80378f3d07fcecc6b
OHTA Shogo <athos0220@gmail.com>
2014-06-30 22:25:29 +0900

    reimplement highlight function with new parser

--- src/genuine_highlighter/core.clj
+++ src/genuine_highlighter/core.clj
@@ -1,16 +1,17 @@
 (ns genuine-highlighter.core
-  (require [genuine-highlighter.parser :refer [parse]]
-           [genuine-highlighter.analyzer :refer [analyze]]
-           [genuine-highlighter.renderer :refer [render]]
-           [clojure.tools.reader.reader-types :refer [string-push-back-reader]]))
+  (:require [genuine-highlighter [parser :as parser]
+                                 [analyzer :as analyzer]
+                                 [renderer :as renderer]]
+            [clojure.java.io :as io]
+            [clojure.string :as str])
+  (:import [java.io BufferedReader]))
 
 (defn highlight
-  ([rule] (highlight rule *in*))
-  ([rule rdr]
-     (->> (parse rdr)
-          analyze
-          (render rule))))
-
-(defn highlight-string [rule s]
-  (let [r (string-push-back-reader s)]
-    (highlight rule r)))
+  ([rule s] (highlight rule s nil))
+  ([rule s unfinished]
+     (let [ast (parser/parse s)]
+       (cond (parser/unfinished? ast)
+             #_=> unfinished
+             (parser/unexpected? ast)
+             #_=> (throw (ex-info "unexpected" {::type :unexpected :data s}))
+             :else (renderer/render rule (analyzer/analyze ast))))))
