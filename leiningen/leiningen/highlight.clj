31df3f1d944ad9db949e3ac4553897df4f13ec44
OHTA Shogo <athos0220@gmail.com>
2014-06-30 22:27:13 +0900

    reimplement lein highlight command

--- leiningen/leiningen/highlight.clj
+++ leiningen/leiningen/highlight.clj
@@ -1,23 +1,33 @@
 (ns leiningen.highlight
   (:require [clojure.main :as m]
+            [genuine-highlighter.core :as hl]
             [genuine-highlighter.parser :as p]
             [genuine-highlighter.analyzer :as a]
             [genuine-highlighter.renderer :as r]
             [genuine-highlighter.converter :as c]
             [genuine-highlighter.decoration-rules.terminal :as t]))
 
-(defn- drop-proceeding-newlines [nodes]
-  (drop-while #(and (= (:type %) :whitespaces)
-                    (= (:raw %) "\n"))
-              nodes))
+(defn- drop-proceeding-newlines [s]
+  (clojure.string/replace s #"^\n+" ""))
 
-(defn- read-and-highlight [in]
-  (let [p (drop-proceeding-newlines (p/parse in))]
-    (printf "  #_== %s\n" (r/render t/colorful-symbols-rule (a/analyze p)))
-    (c/convert p)))
+(defn- read-and-highlight [^java.io.BufferedReader in]
+  (loop [code ""]
+    (let [line (.readLine in)
+          code' (str code \newline line)]
+      (if-let [result (hl/highlight t/colorful-symbols-rule code')]
+        (let [result (->> (clojure.string/split result #"\n")
+                          (drop-while #(= "" %))
+                          (map #(str "  #_== " %))
+                          (clojure.string/join \newline))]
+          (println result)
+          (flush)
+          (read-string code'))
+        (do (print "  #_=> ")
+            (flush)
+            (recur code'))))))
 
 (defn highlight
   "Highlight Clojure source code."
   [project & args]
-  (m/repl :read (fn [_ _] (read-and-highlight *in*))
+  (m/repl :read (fn [_ _] (read-and-highlight (clojure.java.io/reader *in*)))
           :need-prompt #(do true)))
