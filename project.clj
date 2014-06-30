d07ca0d77f8fc3ef8eb202334c4b7103703e640b
OHTA Shogo <athos0220@gmail.com>
2014-06-30 23:55:47 +0900

    add some additional dependencies for lein highlight browse

--- project.clj
+++ project.clj
@@ -4,6 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojars.trptcolin/sjacket "0.1.4" :exclusions [[org.clojure/clojure]]]]
+                 [org.clojars.trptcolin/sjacket "0.1.4" :exclusions [[org.clojure/clojure]]]
+                 ;; for lein highlight browse functionality
+                 [compojure "1.1.8"]
+                 [hiccup "1.0.5"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]]
                    :source-paths ["src" "dev" "examples"]}})
