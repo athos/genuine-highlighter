3ca5db809efaf1e03cfd151a620f5eac295d8744
OHTA Shogo <athos0220@gmail.com>
2014-09-11 22:53:14 +0900

    Add some detailed information to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject genuine-highlighter "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "a macro-aware syntax highlighter for Clojure"
+  :url "https://github.com/athos/genuine-highlighter"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
@@ -13,4 +13,5 @@
                  [hiccup "1.0.5"]]
   :resource-paths ["resources"]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]]
-                   :source-paths ["src" "dev" "examples"]}})
+                   :source-paths ["src" "dev" "examples"]}}
+  :global-vars {*warn-on-reflection* true})
