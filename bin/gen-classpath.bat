5a007ea1088f9adcc9d166802513d2ee0a93248b
OHTA Shogo <athos0220@gmail.com>
2015-07-04 22:10:09 +0900

    Remove Leiningen plugin-related stuff

--- bin/gen-classpath.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-for /f "usebackq tokens=*" %%p in (`lein classpath`) do set CLPATH=%%p;leiningen
-echo %CLPATH% > .lein-classpath
--- project.clj
+++ project.clj
@@ -6,17 +6,10 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [symbol-analyzer "0.1.0"]
                  [org.clojars.trptcolin/sjacket "0.1.4" :exclusions [[org.clojure/clojure]]]
-                 [org.clojure/core.match "0.2.2"]
-                 ;; for lein highlight browse functionality
-                 [compojure "1.3.4" :exclusions [instaparse]]
-                 [instaparse "1.4.1"]
-                 [ring/ring-jetty-adapter "1.3.2"]
-                 [ring/ring-devel "1.3.2"]
-                 [hiccup "1.0.5"]]
-  :resource-paths ["resources"]
+                 [org.clojure/core.match "0.2.2"]]
   :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :dev {:dependencies [[org.clojure/tools.namespace "0.2.10"]]
-                   :source-paths ["src" "dev" "examples"]}}
+                   :source-paths ["src" "dev"]}}
   :aliases {"all" ["with-profile" "dev:1.5"]}
   :global-vars {*warn-on-reflection* true})
